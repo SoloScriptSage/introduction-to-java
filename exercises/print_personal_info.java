b0d19ed6b8bd1b060fc28c4f18a2815e04f55dd8
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2024-04-04 23:35:06 -0500

    Create print_personal_info.java

---
+++ exercises/print_personal_info.java
@@ -0,0 +1,11 @@
+public class MyInformation {
+    public static void main(String[] args) {
+        String name = "Vladyslav Hirchuk";
+        int age = 20;
+        String favorite_color = "Black";
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Favorite Color: " + favorite_color);
+    }
+}
